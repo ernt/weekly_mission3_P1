5958c2f2b8fa936f7a17df18c2db5ecc64c24573
Ernt <ernestn@ciencias.unam.mx>
2022-05-21 19:07:21 -0500

    Final changes

---
+++ my_launchx_app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const ernt = new MissionCommander("Ernesto")
+const lala = new MissionCommander("Laura")
+
+console.log(carlo.name)
+console.log(ernt.name)
+console.log(lala.name)
